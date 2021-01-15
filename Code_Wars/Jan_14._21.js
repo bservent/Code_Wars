17ea9dffe84362ec87b546adf9630a3d3353918a
Brittany Servent <bservent@gmail.com>
2021-01-14 17:45:32 -0800

    3 edabit probs

--- Code_Wars/Jan_14._21.js
+++ Code_Wars/Jan_14._21.js
@@ -49,3 +49,52 @@ function has_bugs(buggy_code) {
 		return "it's a good day"
 	}
 }
+
+/* Array Indexing
+Given an index and an array, return the value of the array with the given index.
+
+Examples
+valueAt([1, 2, 3, 4, 5, 6], 10 / 2) ➞ 6
+
+valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2) ➞ 5
+
+valueAt([1, 2, 3, 4], 6.535355314 / 2) ➞ 4
+Notes
+Math.floor() can be helpful. */
+
+function valueAt(arr, i) {
+	return Math.floor(i) + 1
+}
+
+/* Nth Even Number
+Create a function that takes a number n and returns the nth even number.
+
+Examples
+nthEven(1) ➞ 0
+// 0 is first even number
+
+nthEven(2) ➞ 2
+// 2 is second even number
+
+nthEven(100) ➞ 198 */
+
+function nthEven(n) {
+	return (n*2) - 2
+}
+
+/* Return a String as an Integer
+Create a function that takes a string and returns it as an integer.
+
+Examples
+stringInt("6") ➞ 6
+
+stringInt("1000") ➞ 1000
+
+stringInt("12") ➞ 12
+Notes
+All numbers will be whole.
+All numbers will be positive. */
+
+function stringInt(str) {
+	return parseInt(str)
+}
