a02e2d33ebd7119b021daa8d41f5f0639025eaaa
Brittany Servent <bservent@gmail.com>
2021-01-20 21:28:11 -0800

    3 more edabit

--- Code_Wars/Jan_20_21.js
+++ Code_Wars/Jan_20_21.js
@@ -50,4 +50,64 @@ function word(s) {
 	return num.indexOf(s)
 }
 
+/* Number of Stickers
+Given a Rubik's Cube with a side length of n, return the number of individual stickers that are needed to cover the whole cube.
+
+Pictures of Rubik's Cubes
+
+The Rubik's cube of side length 1 has 6 stickers.
+The Rubik's cube of side length 2 has 24 stickers.
+The Rubik's cube of side length 3 has 54 stickers.
+Examples
+howManyStickers(1) ➞ 6
+
+howManyStickers(2) ➞ 24
+
+howManyStickers(3) ➞ 54 */
+
+function howManyStickers(n) {
+	return 6*Math.pow(n,2)
+}
+
+/* Minimal VI: Ternary Operator
+In the Code tab you will find code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
+
+Write a function that returns the strings:
+
+"both" if both given booleans a and b are true.
+"first" if only a is true.
+"second" if only b is true .
+"neither" if both a and b are false.
+Tips
+If-else statements can be written as a oneliner using Javascript's ternary operator.
+
+For example, the code:
+
+function startswith(name) {
+  if ("AEIOU".includes(name[0])) {
+    return "vowel"
+  } else {
+    return "consonant"
+  }
+}
+Can be simplified to:
+
+function startswith(name) {
+  return "AEIOU".includes(name[0]) ? "vowel" : "consonant"
+} */
+
+const areTrue = (a, b) => a & b ? 'both' : a ? 'first' : b ? 'second': 'neither'
+
+/* Find the Index (Part 1)
+Create a function that finds the index of a given item.
+
+Examples
+search([1, 5, 3], 5) ➞ 1
+
+search([9, 8, 3], 3) ➞ 2
+
+search([1, 2, 3], 4) ➞ -1 */
+
+const index = (arr, item) => arr.indexOf(item)
+
 
