41456c2aca5d7b60db44e5d827148f66bed044e1
Brittany Servent <bservent@gmail.com>
2021-01-25 20:51:58 -0800

    3 more edabit probs

--- Code_Wars/Jan_25_21.js
+++ Code_Wars/Jan_25_21.js
@@ -54,3 +54,76 @@ function numberArgs() {
 }
 
 
+/* Sum Fractions
+Create a function that takes an array containing nested arrays as an argument. Each subarray has 2 elements. The first element is the numerator and the second element is the denominator. Return the sum of the fractions rounded to the nearest whole number.
+
+Examples
+sumFractions([[18, 13], [4, 5]]) ➞ 2
+
+sumFractions([[36, 4], [22, 60]]) ➞ 9
+
+sumFractions([[11, 2], [3, 4], [5, 4], [21, 11], [12, 6]]) ➞ 11 */
+
+function sumFractions(arr) {
+	return Math.round(arr.map(v=> v[0]/v[1]).reduce((a,v) => a+v))
+}
+
+/* Find the Bug: Returning the Container
+The packaging system is running wild! The candy is lying loose all over in the warehouse, the cereal is missing, and bread is stuffed in a bottle. What is going on here? The candy should be in plastic and the bread should be in a bag.
+
+The packaging machine is running the getContainer() function to retrieve the container of a product. But something is not right...
+
+Examples
+getContainer("Bread") ➞ "bag"
+
+getContainer("Beer") ➞ "bottle"
+
+getContainer("Candy") ➞ "plastic"
+
+getContainer("Cheese") ➞ null */
+
+function getContainer(product) {
+	let container
+  switch (product) {
+		case "Bread":
+			container = "bag"
+			break;
+		case "Beer":
+		case "Milk":
+			container = "bottle"
+			break;
+		case "Cerials":
+			container = "box"
+			break;
+		case "Eggs":
+			container = "carton"
+			break;
+		case "Candy":
+			container = "plastic"
+			break;
+		default:
+			container = null
+  }
+  
+/* Minimal IX: This or That
+Check the principles of minimalist code in the intro to the first challenge.
+
+In the Code tab you will find a code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
+
+Write a function that returns the first truthy argument passed to the function. If all arguments are falsy, return the string "not found". The function will be called with a minimum of one and a maximum of four arguments: a, b, c, d.
+
+Tips
+The logical OR operator || can be used to assign or return the first truthy value among two or more elements. If no truthy value is found, the last element will be returned.
+
+For example, the code:
+
+function oneOfThese(a, b, c) {
+    if (a) return a;
+    else if (b) return b;
+    else return c;
+}       
+Can be simplified to:
+
+const oneOfThese = (a, b, c) => a || b || c; */
+
+const firstOne = (a, b = 0, c = 0, d = 0) => a || b || c || d || 'not found'
