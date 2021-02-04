6583ad1a3f50c76e523afa49c2b743d4bd5431de
Brittany Servent <bservent@gmail.com>
2021-02-04 12:14:50 -0800

    1 problems

--- Code_Wars/Feb_4_.21.js
+++ Code_Wars/Feb_4_.21.js
@@ -111,3 +111,20 @@ isTrue("15>4") ➞ true */
 function isTrue(relation) {
   return eval(relation.replace('=', '==='))
 }
+
+/* Filter out Strings from an Array
+Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+
+Examples
+filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+
+filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+
+filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123] */
+
+function filterArray(arr) {
+  return arr.filter(n => typeof n === 'number');
+}
+
+
+
