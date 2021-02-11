d899008690d20bbbd4fab13a6440ed301098cc9c
Brittany Servent <bservent@gmail.com>
2021-02-10 21:04:58 -0800

    edabit probs

--- Code_Wars/Feb_10_21.js
+++ Code_Wars/Feb_10_21.js
@@ -57,3 +57,25 @@ oppositeHouse(3, 5) ➞ 8 */
 const oppositeHouse = (house, n) => n*2+1-house
 
 
+/* Folding a Piece of Paper
+Create a function that returns the thickness (in meters) of a piece of paper after folding it n number of times. The paper starts off with a thickness of 0.5mm.
+
+Examples
+numLayers(1) ➞ "0.001m"
+// Paper folded once is 1mm (equal to 0.001m)
+
+numLayers(4) ➞ "0.008m"
+// Paper folded 4 times is 8mm (equal to 0.008m)
+
+numLayers(21) ➞ "1048.576m"
+// Paper folded 21 times is 1048576mm (equal to 1048.576m) */
+
+function numLayers(n) {
+	let count = 0;
+	let i = .0005;
+	while(count < n){
+		i *= 2;
+		count++
+	}
+	return `${i}m`;
+}
