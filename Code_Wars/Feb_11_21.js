47b61eb14df4bc43e558330453b7d2da06554f63
Brittany Servent <bservent@gmail.com>
2021-02-11 20:13:44 -0800

    Lodash drop from array

--- Code_Wars/Feb_11_21.js
+++ Code_Wars/Feb_11_21.js
@@ -48,3 +48,19 @@ const signYourName = (obj) =>{
 	obj.spouseSignature = "Nice Try"
   return obj;
 }
+
+/* Learn Lodash (4): _.dropRight, Drop the Last Elements of an Array
+According to the lodash documentation, _.dropRight Creates a slice of an array with n elements dropped from the end.
+
+This challenge requires you to write your own version of this function without using lodash so that you can better understand it works.
+
+Examples
+dropRight([1, 2, 3]) ➞ [1, 2]
+
+dropRight([1, 2, 3], 2) ➞ [1]
+
+dropRight([1, 2, 3], 5) ➞ []
+
+dropRight([1, 2, 3], 0) ➞ [1, 2, 3] */
+
+dropRight = (a, n = 1) => a.splice(0, a.length - n);
