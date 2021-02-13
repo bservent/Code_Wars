2e0277e32240f773576efa308b8ec4f319bb6a6a
Brittany Servent <bservent@gmail.com>
2021-02-12 16:49:17 -0800

    sorting objects in arry

--- Code_Wars/Feb_12_21.js
+++ Code_Wars/Feb_12_21.js
@@ -17,4 +17,20 @@ Create a REGEXP that matches any characters except letters, digits and spaces. Y
 
  const REGEXP = /[^a-z^1-9]/gi
 
- 
+/*  Drink Sorting
+You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+Assume that the following array of drink objects needs to be sorted:
+
+drinks = [
+  {name: "lemonade", price: 50},
+  {name: "lime", price: 10}
+]
+The output of the sorted drinks object will be:
+
+Examples
+sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}] */
+
+function sortDrinkByPrice(drinks) {
+	return drinks.sort((a,b)=> a.price - b.price)
+}
