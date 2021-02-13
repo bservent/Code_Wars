1691e50bfc0920a237e8051ff01d65aadc56b51e
Brittany Servent <bservent@gmail.com>
2021-02-13 13:28:49 -0800

    object values

---
+++ Code_Wars/Feb_13_21.js
@@ -0,0 +1,28 @@
+/* 2/12/21 CodeWars */
+
+//////////////////////////////////*
+
+/* Given an object of how many more pages each ink color can print, output the maximum number of pages the printer can print before any of the colors run out.
+
+Examples
+inkLevels({
+  "cyan": 23,
+  "magenta": 12,
+  "yellow": 10
+}) ➞ 10
+
+inkLevels({
+  "cyan": 432,
+  "magenta": 543,
+  "yellow": 777
+}) ➞ 432
+
+inkLevels({
+  "cyan": 700,
+  "magenta": 700,
+  "yellow": 0
+}) ➞ 0 */
+
+function inkLevels(inks) {
+  return Math.min(...Object.values(inks))
+}
