14a97b68bcd4801aebe4a51580a97ff538fa7a2c
Brittany Servent <bservent@gmail.com>
2021-02-12 17:36:16 -0800

    find vowels

--- Code_Wars/Feb_12_21.js
+++ Code_Wars/Feb_12_21.js
@@ -69,3 +69,16 @@ function doubleChar(str) {
   return str.split('').map(x=>x+x).join('')
 }
 
+/* How Many Vowels?
+Create a function that takes a string and returns the number (count) of vowels contained within it.
+
+Examples
+countVowels("Celebration") ➞ 5
+
+countVowels("Palm") ➞ 1
+
+countVowels("Prediction") ➞ 4 */
+
+function countVowels(str) {
+  return str.match(/[aeiou]/g).length;
+}
