831ecdeb249b9105a5eddf0bad67f7cf4e31c5b3
Brittany Servent <bservent@gmail.com>
2021-02-12 17:25:30 -0800

    repeat character in array

--- Code_Wars/Feb_12_21.js
+++ Code_Wars/Feb_12_21.js
@@ -55,3 +55,17 @@ function findHighest(arr){
 	return max;
 }
 
+/* Repeating Letters
+Create a function that takes a string and returns a string in which each character is repeated once.
+
+Examples
+doubleChar("String") ➞ "SSttrriinngg"
+
+doubleChar("Hello World!") ➞ "HHeelllloo  WWoorrlldd!!"
+
+doubleChar("1234!_ ") ➞ "11223344!!__  " */
+
+function doubleChar(str) {
+  return str.split('').map(x=>x+x).join('')
+}
+
