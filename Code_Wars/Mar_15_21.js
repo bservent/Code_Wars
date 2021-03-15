00bf3cd86f3b890d693fa8390b6191f28b139754
Brittany Servent <bservent@gmail.com>
2021-03-15 12:36:56 -0700

    sort function probs

--- Code_Wars/Mar_15_21.js
+++ Code_Wars/Mar_15_21.js
@@ -52,4 +52,23 @@ let promise = new Promise((resolve, reject) => {
     resolve = ('britt')
   }, 1000)
   });
-  
+
+/*   Sort an Array by String Length
+Create a function that takes an array of strings and return an array, sorted from shortest to longest.
+
+Examples
+sortByLength(["Google", "Apple", "Microsoft"])
+➞ ["Apple", "Google", "Microsoft"]
+
+sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"])
+➞ ["Raphael", "Leonardo", "Donatello", "Michelangelo"]
+
+sortByLength(["Turing", "Einstein", "Jung"])
+➞ ["Jung", "Turing", "Einstein"] */
+
+function sortArray(arr) {
+  return arr.sort((a,b) => a.length - b.length)
+}
+
+
+
