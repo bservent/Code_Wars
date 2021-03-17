47b44a46c9401d305cabc4d368b5fdf08521db14
Brittany Servent <bservent@gmail.com>
2021-03-17 13:57:25 -0700

    match problem

--- Code_Wars/Mar_17_21.js
+++ Code_Wars/Mar_17_21.js
@@ -13,3 +13,18 @@ num_of_digits(0) ➞ 1 */
 function num_of_digits(num) {
 	return `${num}`.match(/\d/g).length;
 }
+
+/* Find the Bomb
+Create a function that finds the word "bomb" in the given string. If found, return "Duck!!!", otherwise return "There is no bomb, relax.".
+
+Examples
+bomb("There is a bomb.") ➞ "Duck!!!"
+
+bomb("Hey, did you think there is a bomb?") ➞ "Duck!!!"
+
+bomb("This goes boom!!!") ➞ "There is no bomb, relax."
+Notes
+"bomb" may appear in different cases (i.e. uppercase, lowercase, mixed). */
+
+const bomb = (str) => str.match(/bomb/gi)? "Duck!!!" : "There is no bomb, relax.";
+
