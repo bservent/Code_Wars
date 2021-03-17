b3f9b827a633e7a4b1977cb0341cb20d69bfee78
Brittany Servent <bservent@gmail.com>
2021-03-16 19:47:59 -0700

    object edabit prob

--- Code_Wars/Mar_16_21.js
+++ Code_Wars/Mar_16_21.js
@@ -29,3 +29,34 @@ function evenOrOdd(str) {
 		return 'Even and Odd are the same';
 	}
 };
+
+/* Emotify the Sentence
+Create a function that changes specific words into emoticons. Given a sentence as a string, replace the words smile, grin, sad and mad with their corresponding emoticons.
+
+word	emoticon
+smile	:D
+grin	:)
+sad	:(
+mad	:P
+Examples
+emotify("Make me smile") ➞ "Make me :D"
+
+emotify("Make me grin") ➞ "Make me :)"
+
+emotify("Make me sad") ➞ "Make me :("
+Notes
+The sentence always starts with "Make me".
+Try to solve this without using conditional statements like if/else or switch. */
+
+function emotify(str) {
+	const obj = {
+		"smile" :	":D",
+		"grin" : ":)",
+		"sad" :	":(",
+		"mad" :	":P"
+	};
+
+	newStr = str.split(' ');
+	newStr[2] = obj[newStr[2]]
+	return newStr.join(' ')
+}
