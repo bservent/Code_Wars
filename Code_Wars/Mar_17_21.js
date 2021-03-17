e942a3df8568f2932d63d82a48eb355fb299dc20
Brittany Servent <bservent@gmail.com>
2021-03-17 14:09:13 -0700

    sort method

--- Code_Wars/Mar_17_21.js
+++ Code_Wars/Mar_17_21.js
@@ -28,3 +28,17 @@ Notes
 
 const bomb = (str) => str.match(/bomb/gi)? "Duck!!!" : "There is no bomb, relax.";
 
+/* Sort an Array by String Length
+Create a function that takes an array of strings and return an array, sorted from shortest to longest.
+
+Examples
+sortByLength(["Google", "Apple", "Microsoft"])
+➞ ["Apple", "Google", "Microsoft"]
+
+sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"])
+➞ ["Raphael", "Leonardo", "Donatello", "Michelangelo"]
+
+sortByLength(["Turing", "Einstein", "Jung"])
+➞ ["Jung", "Turing", "Einstein"] */
+
+const sortByLength = (arr) => arr.sort((a, b) => a.length - b.length)
