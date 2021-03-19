38bc259f4bf98a171a6cb0d2e9e6a54aebfdc971
Brittany Servent <bservent@gmail.com>
2021-03-18 18:30:28 -0700

    sort method prob

--- Code_Wars/Mar_18_21.js
+++ Code_Wars/Mar_18_21.js
@@ -22,3 +22,18 @@ function arrayOperation(x, y, n) {
 	}
 	return newArray
 }
+
+/* Sort an Array by String Length
+Create a function that takes an array of strings and return an array, sorted from shortest to longest.
+
+Examples
+sortByLength(["Google", "Apple", "Microsoft"])
+➞ ["Apple", "Google", "Microsoft"]
+
+sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"])
+➞ ["Raphael", "Leonardo", "Donatello", "Michelangelo"]
+
+sortByLength(["Turing", "Einstein", "Jung"])
+➞ ["Jung", "Turing", "Einstein"] */
+
+const sortByLength = (arr) => arr.sort((a,b) => (a.length - b.length));
