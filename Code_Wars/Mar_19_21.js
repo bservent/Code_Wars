8bbce0326d306910c3f7b5caff6c7a831bea1f9d
Brittany Servent <bservent@gmail.com>
2021-03-19 19:26:16 -0700

    regex

--- Code_Wars/Mar_19_21.js
+++ Code_Wars/Mar_19_21.js
@@ -68,3 +68,22 @@ function removeVowels(str) {
 	}).join('');
 }
 
+/* Recursion: Factorials
+Write a function that calculates the factorial of a number recursively.
+
+Examples
+factorial(5) ➞ 120
+
+factorial(3) ➞ 6
+
+factorial(1) ➞ 1
+
+factorial(0) ➞ 1 */
+
+function factorial(n) {
+	if (n === 0) {
+		return 1
+	}else{
+		return n * factorial(n -1)
+	}
+}
