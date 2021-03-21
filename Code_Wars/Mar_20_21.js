fdaf2214ef6478f555d4cc25a08a7137f5e9b830
Brittany Servent <bservent@gmail.com>
2021-03-20 20:01:35 -0700

    problem

--- Code_Wars/Mar_20_21.js
+++ Code_Wars/Mar_20_21.js
@@ -27,3 +27,22 @@ function indexShuffle(str) {
 	return even + odd
 }
 
+/* Is the Number Symmetrical?
+Create a function that takes a number as an argument and returns true or false depending on whether the number is symmetrical or not. A number is symmetrical when it is the same as its reverse.
+
+Examples
+isSymmetrical(7227) ➞ true
+
+isSymmetrical(12567) ➞ false
+
+isSymmetrical(44444444) ➞ true
+
+isSymmetrical(9939) ➞ false
+
+isSymmetrical(1112111) ➞ true */
+
+function isSymmetrical(num) {
+	const numArr = String(num);
+  return numArr.split('').reverse().join('') === numArr;
+}
+
