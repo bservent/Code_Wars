a40e1d8daa0df60573ca398d12c9b9adb5202904
Brittany Servent <bservent@gmail.com>
2021-03-24 15:25:04 -0700

    recursion

--- Code_Wars/Mar_24_21.js
+++ Code_Wars/Mar_24_21.js
@@ -25,3 +25,20 @@ function hashPlusCount(str) {
 	return [hash, plus]
 		}
 
+/*     Return the Factorial
+    Create a function that takes an integer and returns the factorial of that integer. That is, the integer multiplied by all positive lower integers.
+    
+    Examples
+    factorial(3) ➞ 6
+    
+    factorial(5) ➞ 120
+    
+    factorial(13) ➞ 6227020800 */
+
+    function factorial(int) {
+      if (int == 0) {
+        return 1;
+      }else {
+        return (int * factorial(int - 1));
+      }
+    }
