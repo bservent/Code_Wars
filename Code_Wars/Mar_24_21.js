52f6f124f3780082ff7b36c2b7fe024871973380
Brittany Servent <bservent@gmail.com>
2021-03-24 15:18:40 -0700

    hash, plus problem

---
+++ Code_Wars/Mar_24_21.js
@@ -0,0 +1,27 @@
+/* Hashes and Pluses
+Create a function that returns the number of hashes and pluses in a string.
+
+Examples
+hashPlusCount("###+") ➞ [3, 1]
+
+hashPlusCount("##+++#") ➞ [3, 3]
+
+hashPlusCount("#+++#+#++#") ➞ [4, 6]
+
+hashPlusCount("") ➞ [0, 0]
+Notes
+Return [0, 0] for an empty string.
+Return in the order of [hashes, pluses]. */
+
+function hashPlusCount(str) {
+	let plus = 0
+	let hash = 0
+	for(let i = 0; i <= str.length; i++)
+		if(str[i] === '+') {
+			 plus++;
+		}else if (str[i] === '#') {
+			 hash++;
+			 }
+	return [hash, plus]
+		}
+
