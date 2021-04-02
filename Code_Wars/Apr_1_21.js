c79e6710b3eb26f9e0cce8ed19723047484ca2a7
Brittany Servent <bservent@gmail.com>
2021-04-01 18:59:28 -0700

    map problem

--- Code_Wars/Apr_1_21.js
+++ Code_Wars/Apr_1_21.js
@@ -16,3 +16,20 @@ isValid("393939") ➞ false */
 function isValid(zip) {
   return zip.length == 5 && !isNaN(zip);
   }
+
+/*   Secret Society
+  A group of friends have decided to start a secret society. The name will be the first letter of each of their names, sorted in alphabetical order.
+  
+  Create a function that takes in an array of names and returns the name of the secret society.
+  
+  Examples
+  societyName(["Adam", "Sarah", "Malcolm"]) ➞ "AMS"
+  
+  societyName(["Harry", "Newt", "Luna", "Cho"]) ➞ "CHLN"
+  
+  societyName(["Phoebe", "Chandler", "Rachel", "Ross", "Monica", "Joey"]) ➞ "CJMPRR" */
+
+  function societyName(friends) {
+    let name = friends.map(name => name.charAt(0))
+    return name.sort().join('')
+  }
