178131e2406b1670b6096ca8bd69e7f7edbfb4c1
Brittany Servent <bservent@gmail.com>
2021-04-06 14:14:18 -0700

    intro to every()

---
+++ Code_Wars/Apr_6_21.js
@@ -0,0 +1,14 @@
+/* Is One Array a Subset of Another?
+Create a function that returns true if the first array is a subset of the second. Return false otherwise.
+
+Examples
+isSubset([3, 2, 5], [5, 3, 7, 9, 2]) ➞ true
+
+isSubset([8, 9], [7, 1, 9, 8, 4, 5, 6]) ➞ true
+
+isSubset([1, 2], [3, 5, 9, 1]) ➞ false */
+
+function isSubset(arr1, arr2) {
+	let result = arr1.every(val => arr2.includes(val))
+	return result
+}
