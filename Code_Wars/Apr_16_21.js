dc69922c0ce20b6d4d3facbc064cada0c07b436b
Brittany Servent <bservent@gmail.com>
2021-04-16 11:58:53 -0700

    arr to number; number to arr

--- Code_Wars/Apr_16_21.js
+++ Code_Wars/Apr_16_21.js
@@ -14,3 +14,28 @@ The length of the string must remain the same as the input. */
 function cardHide(card) {
 	return card.replace(/\d(?=\d{4})/g, "*");
 }
+
+/* Numbers to Arrays and Vice Versa
+Write two functions:
+
+toArray(), which converts a number to an array of its digits.
+toNumber(), which converts an array of digits back to its number.
+Examples
+toArray(235) ➞ [2, 3, 5]
+
+toArray(0) ➞ [0]
+
+toNumber([2, 3, 5]) ➞ 235
+
+toNumber([0]) ➞ 0
+Notes
+All test cases will be weakly positive numbers: >= 0 */
+
+function toArray(num) {
+	return num.toString().split('').map(x => parseInt(x))
+}
+
+function toNumber(arr) {
+	return Number(arr.join(''))
+}
+
