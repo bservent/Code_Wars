6ab1f1bb024d5e18d7ae4cb07173369208292e0c
Brittany Servent <bservent@gmail.com>
2021-04-16 12:26:06 -0700

    regex

--- Code_Wars/Apr_16_21.js
+++ Code_Wars/Apr_16_21.js
@@ -39,3 +39,18 @@ function toNumber(arr) {
 	return Number(arr.join(''))
 }
 
+/* Transforming Words into Binary Strings
+Write a function that transforms all letters from [a, m] to 0 and letters from [n, z] to 1 in a string.
+
+Examples
+convertBinary("house") ➞ "01110"
+
+convertBinary("excLAIM") ➞ "0100000"
+
+convertBinary("moon") ➞ "0111" */
+
+function convertBinary(str) {
+	return str
+		.replace(/[a-m]/gi, '0') 
+		.replace(/[n-z]/gi, '1');
+}
