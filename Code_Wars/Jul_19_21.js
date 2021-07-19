4aece2bc033efb329391a2a8b1c3ec0958770e20
Brittany Servent <bservent@gmail.com>
2021-07-19 15:05:41 -0700

    Length of Last word off of Leetcode

--- Code_Wars/Jul_19_21.js
+++ Code_Wars/Jul_19_21.js
@@ -13,9 +13,41 @@ var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
       const diff = target - nums[i];
       const diffIndex = nums.indexOf(diff);
-      if (diffIndex !== -1 && diffIndex !== index) {
-        return [index, diffIndex]
+      if (diffIndex !== -1 && diffIndex !== i) {
+        return [i, diffIndex]
       }
     }
 };
+
+/* Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+
+Example 2:
+
+Input: s = " "
+Output: 0 */
+
+s = "Hello World"
+
+var lengthOfLastWord = function(s) {
+  let trimTheFat = s.trim()
+  let words = trimTheFat.split(' ')
+  if (words[words.length-1] === ""){
+      words.pop()
+      return words.length
+  } else {
+      let lastWord = words[words.length-1]
+      return lastWord.length
+  }
+}
+
+
 
