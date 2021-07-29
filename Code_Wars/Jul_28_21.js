c7e8f6d2d770ee308f8a615773f7ea28e2108d75
Brittany Servent <bservent@gmail.com>
2021-07-28 20:00:08 -0700

    modulo of numbers

--- Code_Wars/Jul_28_21.js
+++ Code_Wars/Jul_28_21.js
@@ -8,8 +8,26 @@ concatName("John", "Doe") ➞ "Doe, John"
 
 concatName("Mary", "Jane") ➞ "Jane, Mary" */
 
-function concatName(firstName, lastName) {
+/* function concatName(firstName, lastName) {
   return firstName + ", " + lastName
 }
 
-console.log(concatName("Aleutian", "Rain"))
+console.log(concatName("Aleutian", "Rain")) */
+
+/* Return the Remainder from Two Numbers
+There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+Examples
+remainder(1, 3) ➞ 1
+
+remainder(3, 4) ➞ 3
+
+remainder(-9, 45) ➞ -9
+
+remainder(5, 5) ➞ 0 */
+
+function remainder(x,y) {
+  return x % y
+}
+
+console.log(remainder(4,4))
