7834375ad7951ee308c4206a9a25daa5fad0b0bc
Brittany Servent <bservent@gmail.com>
2021-08-05 12:59:39 -0700

    fizzbuzz

---
+++ Code_Wars/Jul_31_21.js
@@ -0,0 +1,5 @@
+//write a function that takes in a string and returns counts of each character in a string
+//input: string
+//output: number of each character (arr)
+
+//psuedocoding: write a function called charCount that takes in string for parameter. Loop through string and add up characters frequency. Return total number of each character. 
