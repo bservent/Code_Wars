49a4dc9cd1a4897b03f1180128776b1479b8f54c
Brittany Servent <bservent@gmail.com>
2021-09-24 13:49:04 -0700

    Math floor/ceil

--- Code_Wars/Sept_24_21.js
+++ Code_Wars/Sept_24_21.js
@@ -24,3 +24,22 @@ function filterArray(arr) {
 
 //typeof operator returns a string indicating the type of data structure. In our case here, if it returned " number" then it would pass the test and wouldn't be filtered out. 
 
+/* Number Split
+Given a number, return an array containing the two halves of the number. If the number is odd, make the rightmost number higher.
+
+Examples
+numberSplit(4) ➞ [2, 2]
+
+numberSplit(10) ➞ [5, 5]
+
+numberSplit(11) ➞ [5, 6]
+
+numberSplit(-9) ➞ [-5, -4]
+Notes
+All numbers will be integers.
+You can expect negative numbers too. */
+
+function numberSplit(n) {
+	const result = (n/2);
+	return [Math.floor(result), Math.ceil(result)];
+}
