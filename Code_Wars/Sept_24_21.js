4dc50e3492b119e0565e33410a4c71d04e8c2a6f
Brittany Servent <bservent@gmail.com>
2021-09-24 13:34:29 -0700

    filter method

---
+++ Code_Wars/Sept_24_21.js
@@ -0,0 +1,26 @@
+/* Filter out Strings from an Array
+Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+
+Examples
+filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+
+filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+
+filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+
+Notes
+Zero is a non-negative integer.
+The given array only has integers and strings.
+Numbers in the array should not repeat.
+The original order must be maintained. */
+
+function filterArray(arr) {
+	const result = arr.filter(val => 
+    typeof(val) === 'number');
+  return result;
+}
+
+//use filter which is a high order function (takes function as an argument) and test value with strict equality to see if it is a number or not. All the elements that pass the test will log in the output result array.
+
+//typeof operator returns a string indicating the type of data structure. In our case here, if it returned " number" then it would pass the test and wouldn't be filtered out. 
+
