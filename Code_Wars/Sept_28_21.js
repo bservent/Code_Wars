fa2ef5d2b5f65d864f75761abcae7176803d047c
Brittany Servent <bservent@gmail.com>
2021-09-28 17:46:17 -0700

    concat

--- Code_Wars/Sept_28_21.js
+++ Code_Wars/Sept_28_21.js
@@ -38,3 +38,18 @@ function addUp(num) {
 }
 
 console.log(addUp(10));
+
+/* Return Something to Me!
+Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+Examples
+giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+giveMeSomething("something") ➞ "something something" */
+
+function giveMeSomething(a) {
+	return "something " + a 
+}
+
