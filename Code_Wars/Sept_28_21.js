0d3fe4ff47e6cb8bcb154d25863d2723715316dc
Brittany Servent <bservent@gmail.com>
2021-09-28 17:33:21 -0700

    recursion prob

--- Code_Wars/Sept_28_21.js
+++ Code_Wars/Sept_28_21.js
@@ -18,3 +18,23 @@ function flip(y) {
 };
 
 console.log(flip(0));
+
+/* Add up the Numbers from a Single Number
+Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+Examples
+addUp(4) ➞ 10
+
+addUp(13) ➞ 91
+
+addUp(600) ➞ 180300 */
+
+function addUp(num) {
+	let result = 0;
+	for(let i = 1; i <= num; i++) {
+		result += i
+	};
+	return result
+}
+
+console.log(addUp(10));
